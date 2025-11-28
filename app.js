a058366713f23965a7e146d25ae71707792ec485
Gustavo <gustavoalmeidasilva31@gmail.com>
2025-11-28 17:30:42 -0300

    função consultarCEP testada e OK

---
+++ app.js
@@ -0,0 +1,28 @@
+const enderecoUsuario = {
+    rua: document.querySelector('#rua'),    
+    bairro: document.querySelector('#bairro'),
+    cidade: document.querySelector('#cidade'),
+    estado: document.querySelector('#estado'),
+    cep: "06395000"
+}
+async function consultarCEP(cep) {
+  const url = await fetch (`https://viacep.com.br/ws/${cep}/json/`);
+  const resposta = await url.json();
+// Variável do tipo objeto para receber os dados do CEP do usuário
+const dadosDoCEP = {
+    rua: resposta.logradouro,
+    bairro: resposta.bairro,
+    cidade: resposta.localidade,
+    estado: resposta.estado,
+    uf: resposta.uf
+}
+
+enderecoUsuario.rua.innerText = dadosDoCEP.rua
+enderecoUsuario.bairro.innerText = dadosDoCEP.bairro
+enderecoUsuario.cidade.innerText = dadosDoCEP.cidade
+enderecoUsuario.cidade.innerText = dadosDoCEP.estado
+console.log(dadosDoCEP)
+// Return resposta
+}
+
+consultarCEP(enderecoUsuario.cep);
